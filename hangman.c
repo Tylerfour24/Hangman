7bf85b00eeb85579752b5ce7660cc0ce28d38cd3
Tyler Gantt <tgantt@localhost.localdomain>
2015-11-09 15:34:04 -0500

    Just a working program that can produce an array of characters

--- hangman.c
+++ hangman.c
@@ -2,5 +2,22 @@
 
 int main(void)
 {
+	char array[8];
+	int i;
+
+	array[0] = 'H';
+	array[1] = 'a';
+	array[2] = 'n';
+	array[3] = 'g';
+	array[4] = 'm';
+	array[5] = 'a';
+	array[6] = 'n';
+
+	for(i = 0; i < 7; i++) {
+		printf("%c", array[i]);
+	}
+
+	printf("\n");
+
 	return 0;
 }
