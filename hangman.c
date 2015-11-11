b4fc899680703f4ec176904d2d9853d5657ac8bd
Tyler Gantt <tgantt@localhost.localdomain>
2015-11-11 18:06:09 -0500

    Program now has definitive win/loss scenarios and images

--- hangman.c
+++ hangman.c
@@ -2,6 +2,7 @@
 #include <string.h>
 
 void setup(int index, char *answer, char *display);
+void result(int correct, int wrongcount);
 
 int main(void)
 {
@@ -14,7 +15,7 @@ int main(void)
 	int compare;
 	int correct;
 
-	answer[0] = 'h';
+	answer[0] = 'h'; //Still need to grab a word from a file
 	answer[1] = 'a';
 	answer[2] = 'n';
 	answer[3] = 'g';
@@ -35,7 +36,7 @@ int main(void)
 		printf("Enter a letter: ");
 		fgets(guess, sizeof(guess)+1, stdin);
 		strncpy(checker, display, sizeof(display)+1); //this is supposed to create a copy of the empty display
-		//checker[index] = display[index];
+
 		for(index = 0; index < 7; index++) {
 
 			if(answer[index] == guess[0]) {
@@ -46,7 +47,7 @@ int main(void)
 		correct = strcmp(display, answer);
 		compare = strcmp(display, checker);
 
-		if(compare == 0) { //if display was not updated (if guess was wrong)
+		if(compare == 0) {
 			wrongcount += 1;
 		}
 
@@ -54,7 +55,7 @@ int main(void)
 			; //Do nothing
 		}
 
-		printf("%d\n", wrongcount);
+		result(correct, wrongcount);
 
 		for(index = 0; index < 7; index++) {
 			printf("%c", display[index]);
@@ -76,5 +77,38 @@ void setup(int index, char *answer, char *display) {
 		printf("%c", display[index]);
 	}
 
-	printf("\n");
+	printf("\n\n");
+}
+
+void result(int correct, int wrongcount) {
+	if(correct == 0) {
+		printf("\n\nYOU WIN!\n\n");
+		printf("   ___\n  |   |\n      |\n      |      O\n      |     /|\\ \n  ____|____ / \\ \n"); //Prints winning screen
+	}
+	else {
+		switch(wrongcount){ //Prints necessary screen based on number of guesses remaining.
+			case(0) :
+				printf("   ___\n  |   |\n      |\n      |\n      |\n  ____|____\n");
+				break;
+			case(1) :
+				printf("   ___\n  |   |\n  O   |\n      |\n      |\n  ____|____\n");
+				break;
+			case(2) :
+				printf("   ___\n  |   |\n \\O   |\n      |\n      |\n  ____|____\n");
+				break;
+			case(3) :
+				printf("   ___\n  |   |\n \\O/  |\n      |\n      |\n  ____|____\n");
+				break;
+			case(4) :
+				printf("   ___\n  |   |\n \\O/  |\n  |   |\n      |\n  ____|____\n");
+				break;
+			case(5) :
+				printf("   ___\n  |   |\n \\O/  |\n  |   |\n /    |\n  ____|____\n");
+				break;
+			case(6) :
+				printf("\n\nYOU LOSE!\n\n");
+				printf("   ___\n  |   |\n \\O/  |\n  |   |\n / \\  |\n  ____|____\n");
+				break;
+		}
+	}
 }
