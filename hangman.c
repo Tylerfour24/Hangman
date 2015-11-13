9671f4c0bce37417370adbe0185e7194dd185c42
Tyler Gantt <tgantt@localhost.localdomain>
2015-11-13 15:50:17 -0500

    program can parse through specified file

--- hangman.c
+++ hangman.c
@@ -1,39 +1,65 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 void setup(int index, char *answer, char *display);
 void result(int correct, int wrongcount);
+void statistics(int correct, int wrongcount);
 
-int main(void) //(int argc, char *argv[])
+int main(void) //(int argc, char *argv[]) once this is ready to accept command line input, if ever
 {
-	char answer[8];
-	char display[8];
-	char checker[8];
-	char guess[2]; //space for one character, the newline, and the null byte
+	srand(time(NULL));
+	char answer[35]={'\0'};
+	char display[35]={'\0'};
+	char checker[35]={'\0'};
+	char guess[2];
 	int index;
 	int wrongcount;
 	int compare;
 	int correct;
+	char *combine;
+	int coin;
+	//int *wins
+	//int *losses
+	//wins = 0;
+	//losses = 0;
+	
+	printf("%s\n", getenv("HOME")); //(debugging?) statement to show that i'm getting an environment variable
+	combine = strncat(getenv("HOME"), "/.words", 35);
+	FILE *userfile = fopen(combine, "r");
+	if(userfile == NULL) {
+		printf("File missing, exiting program.\n");
+		exit(0);
+	}
 
-	answer[0] = 'h'; //Still need to grab a word from a file
-	answer[1] = 'a';
-	answer[2] = 'n';
-	answer[3] = 'g';
-	answer[4] = 'm';
-	answer[5] = 'a';
-	answer[6] = 'n';
+	while(!EOF){ //if not at end of file
+		coin = rand(); //generate random number
+		coin %= 2; //that is equal to (0?), 1, or (2?)
+		if(coin == 1) { //if random number generated is 1
+			fgets(answer, 35, userfile); //read in next line
+			break;
+		}
+		else {
+		; //do nothing
+		}
+	}
 
-/*picking random line from file of unknown length
-row 1 - 100%
-row 2 - 50%
-row 3 - 33%
+	answer[0] = 's'; //Still need to grab a word from a file (and make a history file)
+	answer[1] = 'c';
+	answer[2] = 'h';
+	answer[3] = 'i';
+	answer[4] = 's';
+	answer[5] = 'm';
+	answer[6] = 's';
 
-0 <= x <= 1
-
-x < 1/n; x
-*/
-	for(index = 0; index < 7; index++) {
-		display[index] = '_';
+	for(index = 0; index < 35; index++) {
+		if(answer[index] == '\0') {
+			break;
+		}
+		else {
+			display[index] = '_';
+		}
 	}
 
 	setup(index, answer, display);
@@ -44,12 +70,12 @@ x < 1/n; x
 
 		printf("Enter a letter: ");
 		fgets(guess, sizeof(guess)+1, stdin);
-		strncpy(checker, display, sizeof(display)+1); //this is supposed to create a copy of the empty display
+		strncpy(checker, display, sizeof(display)+1);
 
 		for(index = 0; index < 7; index++) {
 
 			if(answer[index] == guess[0]) {
-				display[index] = guess[0]; //alter display if the guessed letter is present
+				display[index] = guess[0];
 			}
 		}
 		
@@ -65,6 +91,7 @@ x < 1/n; x
 		}
 
 		result(correct, wrongcount);
+
 		printf("\n");
 
 		for(index = 0; index < 7; index++) {
@@ -72,12 +99,21 @@ x < 1/n; x
 		}
 		printf("\n\n");
 	}
+
+	if(correct != 0) {
+		printf("Correct answer: ");
+		for(index = 0; index < 7; index++) {
+			printf("%c", answer[index]);
+		}
+		printf("\n\n");
+	}
+
 	return 0;
 }
 
-void setup(int index, char *answer, char *display) {
+void setup(int index, char *answer, char *display) { //Starts the game
 
-	for(index = 0; index < 7; index++) {
+	for(index = 0; index < 7; index++) { //Remove this before submitting program
 		printf("%c", answer[index]);
 	}
 
@@ -90,10 +126,10 @@ void setup(int index, char *answer, char *display) {
 	printf("\n\n");
 }
 
-void result(int correct, int wrongcount) {
+void result(int correct, int wrongcount) { //Handles guess and win/loss images
 	if(correct == 0) {
 		printf("\n\nYOU WIN!!\n\n");
-		printf("   ___\n  |   |\n      |\n      |     \\O/\n      |      |   \n  ____|____ / \\ \n"); //Prints winning screen
+		printf("   ___\n  |   |\n      |\n      |     \\0/\n      |      |   \n  ____|____ / \\ \n"); //Prints winning screen
 		switch(wrongcount) {
 			case(0) :
 				printf("You missed no guesses!");
@@ -121,19 +157,19 @@ void result(int correct, int wrongcount) {
 				printf("   ___\n  |   |\n      |\n      |\n      |\n  ____|____\n");
 				break;
 			case(1) :
-				printf("   ___\n  |   |\n  O   |\n      |\n      |\n  ____|____\n");
+				printf("   ___\n  |   |\n  0   |\n      |\n      |\n  ____|____\n");
 				break;
 			case(2) :
-				printf("   ___\n  |   |\n  O   |\n  |   |\n      |\n  ____|____\n");
+				printf("   ___\n  |   |\n  0   |\n  |   |\n      |\n  ____|____\n");
 				break;
 			case(3) :
-				printf("   ___\n  |   |\n  O   |\n /|   |\n      |\n  ____|____\n");
+				printf("   ___\n  |   |\n  0   |\n /|   |\n      |\n  ____|____\n");
 				break;
 			case(4) :
-				printf("   ___\n  |   |\n  O   |\n /|\\  |\n      |\n  ____|____\n");
+				printf("   ___\n  |   |\n  0   |\n /|\\  |\n      |\n  ____|____\n");
 				break;
 			case(5) :
-				printf("   ___\n  |   |\n  O   |\n /|\\  |\n /    |\n  ____|____\n");
+				printf("   ___\n  |   |\n  0   |\n /|\\  |\n /    |\n  ____|____\n");
 				break;
 			case(6) :
 				printf("\n\nYOU LOSE!\n\n");
@@ -143,4 +179,28 @@ void result(int correct, int wrongcount) {
 	}
 }
 
-/*Should the making of the history file be included in the makefile? That would make sense.*/
+/*void statistics(int correct, int wrongcount, int wins, int losses) {
+	if(correct == 0) {
+		*wins++;
+	}
+	else if (wrongcount == 6) {
+		*losses++;
+	}
+}*/
+
+	/*if(argc != 2) {
+		FILE *fopen(~/.words, r);
+	}
+	else {
+		FILE *fopen(argv[1], r);
+	}*/
+
+/*picking random line from file of unknown length
+row 1 - 100%
+row 2 - 50%
+row 3 - 33%
+
+0 <= x <= 1
+
+x < 1/n; x
+*/
